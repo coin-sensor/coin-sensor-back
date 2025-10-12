ec52d5c88aec5fb1c4a219273f7d0bc6b993e40c
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-10-12 15:26:14 +0900

    feat: DB, redis 기본세팅

---
+++ src/main/java/com/coinsensor/common/config/DataSourceConfig.java
@@ -0,0 +1,92 @@
+package com.coinsensor.common.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class DataSourceConfig {
+
+	@Value("${spring.datasource.dbcp2.username}")
+	private String username;
+
+	@Value("${spring.datasource.dbcp2.password}")
+	private String password;
+
+	/**
+	 * PoolSize = Tn × ( Cm - 1 ) + ( Tn / 2 )
+	 * thread count : 12
+	 * simultaneous connection count : 2
+	 * pool size : 12 * ( 2 – 1 ) + (12 / 2) = 18
+	 */
+
+	@Bean
+	@Profile("dev")
+	public DataSource devDataSource(@Value("${spring.datasource.url}") String url) {
+		return createDataSource(url);
+	}
+
+	@Bean
+	@Profile("prod")
+	public DataSource prodDataSource(@Value("${spring.datasource.url}") String url) {
+		return createDataSource(url);
+	}
+
+	// @Bean
+	// @Profile("prod")
+	// public DataSource routingDataSource(
+	// 	@Qualifier("writeDataSource") DataSource writeDataSource,
+	// 	@Qualifier("readDataSource") DataSource readDataSource) {
+	// 	RoutingDataSource routingDataSource = new RoutingDataSource();
+	// 	Map<Object, Object> dataSourceMap = new HashMap<>();
+	// 	dataSourceMap.put(DataSourceContextHolder.WRITE, writeDataSource);
+	// 	dataSourceMap.put(DataSourceContextHolder.READ, readDataSource);
+	//
+	// 	routingDataSource.setTargetDataSources(dataSourceMap);
+	// 	routingDataSource.setDefaultTargetDataSource(writeDataSource);
+	//
+	// 	return routingDataSource;
+	// }
+	//
+	// @Primary
+	// @Bean
+	// @Profile("prod")
+	// public DataSource dataSource(DataSource routingDataSource) {
+	// 	return new LazyConnectionDataSourceProxy(routingDataSource);
+	// }
+	//
+	// @Bean
+	// @Profile("prod")
+	// public DataSource writeDataSource(@Value("${spring.datasource.router.url.rw}") String url) {
+	// 	return createDataSource(url);
+	// }
+	//
+	// @Bean
+	// @Profile("prod")
+	// public DataSource readDataSource(@Value("${spring.datasource.router.url.ro}") String url) {
+	// 	return createDataSource(url);
+	// }
+
+	private BasicDataSource createDataSource(String url) {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+		// 💡 기존 설정 그대로 반영
+		dataSource.setInitialSize(18);
+		dataSource.setMaxTotal(18);
+		dataSource.setMaxIdle(18);
+		dataSource.setMinIdle(18);
+
+		dataSource.setTestOnBorrow(true);
+		dataSource.setValidationQuery("SELECT 1");
+
+		return dataSource;
+	}
+}
---
+++ src/main/java/com/coinsensor/common/config/RedisConfig.java
@@ -0,0 +1,124 @@
+package com.coinsensor.common.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String hostname;
+
+	@Value("${spring.data.redis.password}")
+	private String password;
+
+	private LettuceConnectionFactory createLettuceConnectionFactory(int dbIndex) {
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+		config.setHostName(hostname);
+		config.setPassword(password);
+		config.setPort(6379);
+		config.setDatabase(dbIndex);
+		return new LettuceConnectionFactory(config);
+	}
+
+	private RedisTemplate<String, Object> createRedisTemplate(RedisConnectionFactory factory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		GenericJackson2JsonRedisSerializer serializer =
+			new GenericJackson2JsonRedisSerializer(objectMapper);
+
+		template.setConnectionFactory(factory);
+		template.setValueSerializer(serializer);
+		template.setHashValueSerializer(serializer);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		return template;
+	}
+
+	@Bean
+	@Primary
+	public RedisConnectionFactory redisConnectionFactory() {
+		return createLettuceConnectionFactory(2);
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		return createRedisTemplate(redisConnectionFactory);
+	}
+
+	@Bean("cacheRedisConnectionFactory")
+	public RedisConnectionFactory cacheRedisConnectionFactory() {
+		return createLettuceConnectionFactory(3);
+	}
+
+	@Bean("cacheRedisTemplate")
+	public RedisTemplate<String, Object> cacheRedisTemplate(
+		@Qualifier("cacheRedisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+		return createRedisTemplate(redisConnectionFactory);
+	}
+
+	@Bean("keywordRedisConnectionFactory")
+	public RedisConnectionFactory keywordRedisConnectionFactory() {
+		return createLettuceConnectionFactory(4);
+	}
+
+	@Bean("keywordRedisTemplate")
+	public RedisTemplate<String, Object> keywordRedisTemplate(
+		@Qualifier("keywordRedisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+		return createRedisTemplate(redisConnectionFactory);
+	}
+
+	@Bean("leaderRedisConnectionFactory")
+	public RedisConnectionFactory leaderRedisConnectionFactory() {
+		return createLettuceConnectionFactory(6);
+	}
+
+	@Bean("leaderRedisTemplate")
+	public RedisTemplate<String, Object> leaderRedisTemplate(
+		@Qualifier("leaderRedisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+		return createRedisTemplate(redisConnectionFactory);
+	}
+
+	@Bean("cacheManager")
+	public RedisCacheManager redisCacheManager(
+		@Qualifier("cacheRedisConnectionFactory") RedisConnectionFactory connectionFactory) {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+		return RedisCacheManager.builder(connectionFactory)
+			.cacheDefaults(config)
+			.build();
+	}
+}
+
