6308a06b65310f3b64eccc65f561b452252458bb
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-11-19 00:05:52 +0900

    feat: 탐지 통계 테이블 제거

--- src/test/java/com/coinsensor/CoinSensorApplicationTests.java
+++ src/test/java/com/coinsensor/CoinSensorApplicationTests.java
@@ -1,22 +1,18 @@
 package com.coinsensor;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import com.coinsensor.detectionstatistics.service.DetectionStatisticsService;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CoinSensorApplicationTests {
 
-	@Autowired
-	private DetectionStatisticsService detectionStatisticsService;
-
-	@Test
-	void testAggregateDetectionStatistics() {
-		detectionStatisticsService.aggregateDetectionStatistics();
-	}
+	// @Autowired
+	// private DetectionStatisticsService detectionStatisticsService;
+	//
+	// @Test
+	// void testAggregateDetectionStatistics() {
+	// 	detectionStatisticsService.aggregateDetectionStatistics();
+	// }
 
 }
