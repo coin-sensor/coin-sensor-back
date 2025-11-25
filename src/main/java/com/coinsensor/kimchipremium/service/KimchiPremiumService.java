1099eb9b3720d4cdabc866166554334b457d015a
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-11-25 23:34:43 +0900

    feat: 불필요 패키지 제거

--- src/main/java/com/coinsensor/kimchipremium/service/KimchiPremiumService.java
+++
@@ -1,7 +0,0 @@
-package com.coinsensor.kimchipremium.service;
-
-import com.coinsensor.kimchipremium.dto.response.KimchiPremiumResponse;
-
-public interface KimchiPremiumService {
-    KimchiPremiumResponse getLatest();
-}
