e3fc8eb0764c59183a34362b4642e9cf8b35c74c
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2026-01-17 19:25:07 +0900

    feat: actuator 추가, ci-cd 추가

---
+++ src/main/java/com/coinsensor/actuator/controller/ActuatorController.java
@@ -0,0 +1,35 @@
+package com.coinsensor.actuator.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/back/actuator")
+public class ActuatorController {
+
+	@Value("${server.port}")
+	private int port;
+
+	@GetMapping("/health")
+	public ResponseEntity<Boolean> backCheckHealth() {
+		return ResponseEntity.ok(true);
+	}
+
+	@GetMapping("/version")
+	public ResponseEntity<String> backCheckVersion() {
+		String version;
+		if (port == 8101 || port == 8102) {
+			version = "blue";
+		} else {
+			version = "green";
+		}
+		return ResponseEntity.ok(version);
+	}
+
+}
