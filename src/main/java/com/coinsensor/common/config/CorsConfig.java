53822d629cc60fe02dc8ab354da56d6bd8e36239
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2026-01-31 16:59:41 +0900

    feat: CORS 설정 수정

--- src/main/java/com/coinsensor/common/config/CorsConfig.java
+++ src/main/java/com/coinsensor/common/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-			.allowedOrigins("https://www.coinsensor.kr", "https://coinsensor.kr", "http://localhost:3000")
+			.allowedOriginPatterns("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true);
